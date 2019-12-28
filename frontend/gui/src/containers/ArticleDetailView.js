b04797ddb408c1b1c6ce4369042538e3f336e7cf
umair.khan <umair.khan@dubizzle.com>
2019-12-28 20:43:25 +0400

    initial commit

---
+++ frontend/gui/src/containers/ArticleDetailView.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Card} from 'antd';
+import axios from 'axios';
+
+class ArticleDetail extends React.Component {
+    state = {
+        article: []    
+    }
+
+    componentDidMount() {
+        const articleId = this.props.match.params.articleId;
+        axios.get(`http://127.0.0.1:8000/api/${articleId}`)
+            .then(res => {
+                this.setState({
+                    article: res.data
+                });
+                console.log(res.data);
+            })
+    }
+
+    render() {
+        return (
+            <Card title={this.state.article.title}>
+                <p>{this.state.article.content}</p>
+            </Card>
+        )
+    }
+}
+
+export default ArticleDetail;
